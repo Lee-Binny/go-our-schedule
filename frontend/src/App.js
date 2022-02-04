f22ba2c68fa757d7b0f1168bc0f35afe3020f4f3
이수빈 <qlsdl9516@gmail.com>
2022-02-05 00:27:05 +0900

    front - 로그인 페이지 레이아웃 설정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
 import 'antd/dist/antd.css';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        {/*<Route path='/signin' element={<Login />} />*/}
+        <Route path='/signin' element={<SignIn />} />
         <Route path='/' element={<Home />} />
       </Routes>
     </BrowserRouter>
