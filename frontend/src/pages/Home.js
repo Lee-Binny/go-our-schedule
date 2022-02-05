ceb91189f3f978283980ea0ba2066c9221a2e644
이수빈 <qlsdl9516@gmail.com>
2022-02-05 20:52:02 +0900

    front - 캘린더, 멤버, 채팅룸 레이아웃 세팅

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -6,19 +6,26 @@ import {
   MessageOutlined,
   DownOutlined
 } from '@ant-design/icons';
+import Calendar from "../components/Calendar/Calendar";
+import Members from "../components/Members/Members";
+import ChatRoom from "../components/ChatRoom/ChatRoom";
 
 const { Header, Content, Sider } = Layout;
 
 const Home = () => {
   const [collapsed, setCollapsed] = useState(false);
-
+  const [selectedMenu, setSelectedMenu] = useState('Calendar');
   const onCollapse = () => {
     setCollapsed(!collapsed);
   };
 
+  const onClickMenu = (event) => {
+    setSelectedMenu(event.key);
+  }
+
   const menu = (
     <Menu>
-      <Menu.Item key="1" >
+      <Menu.Item key="Group1" >
         Group 1
       </Menu.Item>
       <Menu.Item key="2">
@@ -31,7 +38,7 @@ const Home = () => {
   );
 
   return (
-    <Layout>
+    <Layout style={{ height: '100vh'}}>
       <Header className="site-layout-background" style={{ padding: 0 }} >
         <Dropdown overlay={menu}>
           <Button style={{
@@ -48,24 +55,24 @@ const Home = () => {
           </Button>
         </Dropdown>
       </Header>
-      <Layout style={{ minHeight: '100vh' }}>
-        <Sider collapsible style={{ backgroundColor: '#ffffff' }} collapsed={collapsed} onCollapse={onCollapse} >
-          <Menu theme="light" defaultSelectedKeys={['1']} mode="inline">
-            <Menu.Item key="1" icon={<CalendarOutlined />}>
+      <Layout style={{ height: 'calc(100% - 64px)' }}>
+        <Sider collapsible style={{ backgroundColor: '#fafafa' }} collapsed={collapsed} onCollapse={onCollapse} >
+          <Menu theme="light" defaultSelectedKeys={[selectedMenu]} onClick={onClickMenu} mode="inline">
+            <Menu.Item key="Calendar" icon={<CalendarOutlined />}>
               Calendar
             </Menu.Item>
-            <Menu.Item key="2" icon={<TeamOutlined />}>
+            <Menu.Item key="Members" icon={<TeamOutlined />}>
               Members
             </Menu.Item>
-            <Menu.Item key="3" icon={<MessageOutlined />}>
+            <Menu.Item key="ChatRoom" icon={<MessageOutlined />}>
               ChatRoom
             </Menu.Item>
           </Menu>
         </Sider>
-        <Layout className="site-layout">
-          <Content style={{ margin: '0 16px' }}>
-            <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-              Bill is a cat.
+        <Layout className="site-layout" style={{ backgroundColor: '#ffffff' }}>
+          <Content style={{ margin: '0 16px', backgroundColor: '#ffffff' }}>
+            <div className="site-layout-background" style={{ padding: 24, height: '100%' }}>
+              { selectedMenu === 'Calendar' ? <Calendar/> : selectedMenu === 'Members' ? <Members/> : <ChatRoom/> }
             </div>
           </Content>
         </Layout>
